4b9777a3e31d5fe8a0b1b0f5457d64b142ae7412
WeimarC <>
2021-10-04 13:29:41 -0300

    Segunda Parte

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,23 @@
 import "./styles/Main.css";
+import Columna from "./Columna";
+import Masonry from "react-masonry-css";
 const Main = () => {
-  return <main></main>;
+  return (
+    <main>
+      <Masonry
+        breakpointCols={3}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {/* array of JSX items */}
+      </Masonry>
+      <Columna />
+      <Columna />
+      <Columna />
+      <Columna />
+      <Columna />
+    </main>
+  );
 };
 
 export default Main;
